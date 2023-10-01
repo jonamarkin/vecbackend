3f6c5b8ff56254e0c2939681b07de30ff5621726
Jonathan Ato Markin <jona10711@gmail.com>
2023-10-01 16:52:07 +0200

    Bulk

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ app.use(cors({
     origin: '*'
 }));
 
+
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./vecapp.json'); // Replace with the actual path to your service account key JSON file
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://vecapp-381db.firebaseio.com' // Replace with your Firebase project URL
+});
+
 app.post('/sms', (req, res) => {
     // Handle the SMS request here
     console.log('Received SMS request:', req.body);
@@ -81,6 +91,72 @@ app.get('/test', (req, res) => {
 });
 
 
+app.post('/sendBulk', (req, res) => {
+    const message = req.body.message;
+
+    //Fetch all numbers from database
+    const db = admin.firestore();
+    const usersRef = db.collection('uat_audiences');
+    const phoneNumbers = [];
+
+    usersRef.get().then((snapshot) => {
+        snapshot.forEach((doc) => {
+            console.log(doc.id, '=>', doc.data());
+            const userdata = doc.data();
+            //Format number
+            let number = userdata.phone;
+            number = fomatNumber(number);
+            phoneNumbers.push(number);
+        });
+    });
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", "Basic aG9wdWN4eXg6cnpxZXB4dWk=");
+
+    var raw = JSON.stringify({
+        "From": "VEC@10",
+        "Recipients": phoneNumbers,
+        "Content": "Hello, Kindly help us save hearts! Contribute easily through this link: https://p.hbtl.co/B5x7Nk. You can use Momo or debit card to pay with this link.\n \n Want to pledge? Just click here: https://forms.gle/9ZH5KyjfBRBYQas4A. \nGod bless you!"
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("https://sms.hubtel.com/v1/messages/batch/simple/send", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .then(res.json({
+            message: 'SMS received and processed successfully!',
+            code: 200
+        }))
+        .catch(error => console.log('error', error));
+
+});
+
+const fomatNumber = (number) => {
+    if (number.startsWith('0') && number.length == 10) {
+        number = number.replace('0', '233');
+    }
+
+    if (number.startsWith('+') && number.length == 13) {
+        number = number.replace('+', '');
+    }
+
+    if (number.startsWith('233') && number.length == 12) {
+        number = number;
+    }
+
+    if (number.includes(' ')) {
+        number = number.replace(' ', '');
+    }
+
+    return number;
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
